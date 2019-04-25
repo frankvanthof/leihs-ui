a993ca1519e381d0ad5c65cc868f38ae69b67614
Max Albrecht <max.albrecht@zhdk.ch>
2019-04-25 16:24:00 +0200

    feat: passwort reset UI

--- src/pages/PasswordForgotSuccessPage.js
+++ src/pages/PasswordForgotSuccessPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import f from 'lodash'
+// import f from 'lodash'
 
 import Navbar from '../components/Navbar'
 import FlashMessages from '../components/FlashMessages'
