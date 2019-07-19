10527d1ad8a62abc284c0809f3d4eb29726d68a7
Max Albrecht <max.albrecht@zhdk.ch>
2019-07-19 11:03:14 +0200

    Update Util.js

--- src/components/Util.js
+++ src/components/Util.js
@@ -1,8 +1,6 @@
 import React, { Fragment as F } from 'react'
 import f from 'lodash'
 
-export const Let = ({ children, ...p }) => <F>{children(p)}</F>
-
 export const breakLinesReact = text =>
   // 'hello\nworld' => hello<br/>world
   !!text &&
@@ -13,3 +11,28 @@ export const breakLinesReact = text =>
       {i + 1 < a.length && <br />}
     </React.Fragment>
   ))
+
+
+export const Let = ({ children, ...p }) => <F>{children(p)}</F>
+                                               
+export const IfLet = ({ children, ...bindings }) => {
+  const keys = Object.keys(bindings)
+  if (keys.length > 1) throw new TypeError('IfLet requires 0 or 1 bindings!')
+  const binding = bindings[keys[0]]
+  return binding ? children(binding) : null
+}
+IfLet.propTypes = {
+  children: PropTypes.func.isRequired
+}
+
+export const IfLetElse = ({ children, ...bindings }) => {
+  const keys = Object.keys(bindings)
+  if (keys.length > 1) throw new TypeError('IfLet requires 0 or 1 bindings!')
+  if (children.length > 2) throw new TypeError('IfLet requires 1 or 2 children!')
+  const binding = bindings[keys[0]]
+  return binding ? children[0](binding) : children[1] ? children[1](binding) : null
+}
+IfLetElse.propTypes = {
+  children: PropTypes.arrayOf(PropTypes.func.isRequired).isRequired
+}
+
