fb2452ba1e34c2e9fc43b20034f87ab41e8441ec
Max Albrecht <max.albrecht@zhdk.ch>
2020-10-15 10:59:46 +0200

    upgrade storybook, dependencies, theme deps

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,15 +1,14 @@
 import { addParameters, addDecorator } from '@storybook/react'
-import { withInfo } from '@storybook/addon-info'
+import { MINIMAL_VIEWPORTS } from '@storybook/addon-viewport';
 import './fake-time'
 
 // add global CSS styles
 import '../src/theme/bootstrap-leihs.css'
 
-// addDecorator(withInfo)
 
-addParameters({
-  // viewport: {
-  //   // viewports: newViewports, // newViewports would be an ViewportMap. (see below for examples)
-  //   // defaultViewport: 'mobile2'
-  // }
-})
+export const parameters = {
+  viewport: {
+    viewports: MINIMAL_VIEWPORTS,
+    defaultViewport: 'mobile2'
+  }
+}
