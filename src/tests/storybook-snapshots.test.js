48ec833f75a406d8d7ebd0553a6ef792f5b3cb44
Max Albrecht <max.albrecht@zhdk.ch>
2020-10-15 13:37:18 +0200

    snapshot tests

--- src/tests/storybook-snapshots.test.js
+++ src/tests/storybook-snapshots.test.js
@@ -1,2 +1,5 @@
 import initStoryshots from '@storybook/addon-storyshots'
-initStoryshots()
+import { puppeteerTest } from '../../test/node_modules/@storybook/addon-storyshots-puppeteer'
+
+initStoryshots({ suite: 'HTML Snapshots' })
+initStoryshots({ suite: 'Visual Snapshots', test: puppeteerTest() })
